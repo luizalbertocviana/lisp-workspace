e02f8a1ecbad7c8d5fd5997bdc74395f274bb47c
Luiz Alberto <luizalbertocviana@gmail.com>
2020-01-12 17:32:51 -0300

    first version of macros.lisp

---
+++ macros.lisp
@@ -0,0 +1,50 @@
+(using "lists")
+
+(defmacro with-gensyms ((&rest symbols) &body body)
+  "assigns a gensym for each arg in a let environment"
+  `(let (,@(loop for symbol in symbols
+                 collect `(,symbol (gensym))))
+     ,@body))
+
+(defmacro pipeline (expr &rest exprs)
+  "anaphoric macro to create a sequence of expressions where it refers to the result of the previous expression"
+  (if (null exprs)
+      expr
+      `(let ((it ,expr))
+         (pipeline ,@exprs))))
+
+(defmacro alias (new-name old-name)
+  "creates an alias to an already defined function or macro"
+  (let ((alias-doc
+          (format nil "~a is an alias for ~a. " new-name old-name))
+        (doc-string (documentation old-name 'function)))
+    `(defmacro ,new-name (&rest args)
+       ,(concatenate 'string alias-doc doc-string)
+       `(,',old-name ,@args))))
+
+(defmacro aliases (&rest args)
+  "creates an alias for each pair of arguments"
+  `(progn ,@(loop for pair in (apply #'pairs args)
+                  collect `(alias ,(car pair) ,(cdr pair)))))
+
+(defmacro fn0 (&body body)
+  "macro to create a lambda with no arguments"
+  `(lambda () ,@body))
+
+(defmacro fn1 (&body body)
+  "anaphoric macro to create a lambda with one _ argument"
+  `(lambda (_) ,@body))
+
+(defmacro fn2 (&body body)
+  "anaphoric macro to create a lambda with arguments _1 and _2"
+  `(lambda (_1 _2) ,@body))
+
+(defmacro compose-predicates (expr)
+  "builds a function where every symbol in expr other than 'and and 'or is turned into a proper function call"
+  (let ((logical-operators '(and or)))
+    (with-gensyms (x)
+      (labels ((transform (p)
+                 (if (member p logical-operators)
+                     p
+                     `(,p ,x))))
+        `(lambda (,x) ,(maptree #'transform expr))))))
