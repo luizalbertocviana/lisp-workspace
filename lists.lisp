b5edec85fd2dc1cead78147f7f404de25606c9b1
Luiz Alberto <luizalbertocviana@gmail.com>
2020-01-17 03:06:32 -0300

    adapted for use defmodule macro

--- lists.lisp
+++ lists.lisp
@@ -1,16 +1,16 @@
-(defun pairs (a b &rest others)
-  "arranges its arguments into a list of consed pairs"
-  (let ((pair (cons a b)))
-    (if (null others)
-        (list pair)
-        (cons pair (apply #'pairs others)))))
+(defmodule ("lists")
 
-(defun maptree (f tree)
-  "returns a copy of tree with each of its non-nil elements transformed by f"
-  (cond ((null tree) nil)
-        ((atom tree) (funcall f tree))
-        ((consp tree)
-         (let ((first (car tree)) (second (cdr tree)))
-           (cons (maptree f first) (maptree f second))))))
+  (defun pairs (a b &rest others)
+    "arranges its arguments into a list of consed pairs"
+    (let ((pair (cons a b)))
+      (if (null others)
+          (list pair)
+          (cons pair (apply #'pairs others)))))
 
-(module "lists")
+  (defun maptree (f tree)
+    "returns a copy of tree with each of its non-nil elements transformed by f"
+    (cond ((null tree) nil)
+          ((atom tree) (funcall f tree))
+          ((consp tree)
+           (let ((first (car tree)) (second (cdr tree)))
+             (cons (maptree f first) (maptree f second)))))))
