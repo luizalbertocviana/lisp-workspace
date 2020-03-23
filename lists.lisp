e8a3dcd6e012166c75bcad5ca1869ed20a85d326
Luiz Alberto <luizalbertocviana@gmail.com>
2020-03-23 14:23:03 -0300

    put symbols inside package

--- lists.lisp
+++ lists.lisp
@@ -1,16 +1,23 @@
-(defmodule ("lists")
+(defpackage :lists
+  (:use :common-lisp)
+  (:export
+     :pairs :maptree))
 
-  (defun pairs (a b &rest others)
-    "arranges its arguments into a list of consed pairs"
-    (let ((pair (cons a b)))
-      (if (null others)
-          (list pair)
-          (cons pair (apply #'pairs others)))))
+(in-package :lists)
 
-  (defun maptree (f tree)
-    "returns a copy of tree with each of its non-nil elements transformed by f"
-    (cond ((null tree) nil)
-          ((atom tree) (funcall f tree))
-          ((consp tree)
-           (let ((first (car tree)) (second (cdr tree)))
-             (cons (maptree f first) (maptree f second)))))))
+(defun pairs (a b &rest others)
+  "arranges its arguments into a list of consed pairs"
+  (let ((pair (cons a b)))
+    (if (null others)
+        (list pair)
+        (cons pair (apply #'pairs others)))))
+
+(defun maptree (f tree)
+  "returns a copy of tree with each of its non-nil elements transformed by f"
+  (cond ((null tree) nil)
+        ((atom tree) (funcall f tree))
+        ((consp tree)
+         (let ((first (car tree)) (second (cdr tree)))
+           (cons (maptree f first) (maptree f second))))))
+
+(modules:module "lists")
