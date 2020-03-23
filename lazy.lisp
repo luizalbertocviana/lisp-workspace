efa9f1e0c1df8771b0d66fc30c1d94005e8ac41b
Luiz Alberto <luizalbertocviana@gmail.com>
2020-03-23 14:25:00 -0300

    put symbols inside package

--- lazy.lisp
+++ lazy.lisp
@@ -1,29 +1,37 @@
-(defmodule ("lazy"
-            :using ("aliases" "macros"))
+(modules:using "aliases" "macros")
 
-  (defmacro thunk (&body body)
-    "creates a plist containing the code to be evaluated and the
+(defpackage :lazy
+  (:use :common-lisp :aliases :macros)
+  (:export
+     :thunk :force :lazy-let))
+
+(in-package :lazy)
+
+(defmacro thunk (&body body)
+  "creates a plist containing the code to be evaluated and the
      result of its evaluation"
-    ``(:result nil :code ,(fn0 ,@body)))
+  ``(:result nil :code ,(fn0 ,@body)))
 
-  (defun force (thunk)
-    "force the evaluation of thunk. If already evaluated, just return
+(defun force (thunk)
+  "force the evaluation of thunk. If already evaluated, just return
      the result"
-    (with-expressions ((code   (getf thunk :code))
-                       (result (getf thunk :result)))
-      (when code
-        (setf result (funcall code))
-        (setf code nil))
-      result))
+  (with-expressions ((code   (getf thunk :code))
+                     (result (getf thunk :result)))
+    (when code
+      (setf result (funcall code))
+      (setf code nil))
+    result))
 
-  (defmacro lazy-let ((&rest bindings) &body body)
-    "creates bindings using implicit thunks"
-    (let ((variables (mapcar #'first bindings))
-          (values    (mapcar #'second bindings)))
-      (let ((gvariables (loop for var in variables
-                              collect (gensym))))
-        `(let (,@(loop for gvar in gvariables and val in values
-                       collect `(,gvar (thunk ,@val))))
-           (with-expressions (,@(loop for var in variables and gvar in gvariables
-                                      collect `(,var (force ,gvar))))
-             ,@body))))))
+(defmacro lazy-let ((&rest bindings) &body body)
+  "creates bindings using implicit thunks"
+  (let ((variables (mapcar #'first bindings))
+        (values    (mapcar #'second bindings)))
+    (let ((gvariables (loop for var in variables
+                            collect (gensym))))
+      `(let (,@(loop for gvar in gvariables and val in values
+                     collect `(,gvar (thunk ,@val))))
+         (with-expressions (,@(loop for var in variables and gvar in gvariables
+                                    collect `(,var (force ,gvar))))
+           ,@body)))))
+
+(modules:module "lazy")
