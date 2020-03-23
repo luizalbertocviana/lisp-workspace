872c4c58754cf55749c417b661e51a44b0943ec6
Luiz Alberto <luizalbertocviana@gmail.com>
2020-03-23 14:23:38 -0300

    put symbols inside package

--- macros.lisp
+++ macros.lisp
@@ -1,59 +1,69 @@
-(defmodule ("macros"
-            :using   ("lists")
-            :used-by ("aliases" "lazy"))
+(modules:using "lists")
 
-  (defmacro with-gensyms ((&rest symbols) &body body)
-    "assigns a gensym for each arg in a let environment"
-    `(let (,@(loop for symbol in symbols
-                   collect `(,symbol (gensym))))
-       ,@body))
+(defpackage :macros
+  (:use :common-lisp :lists)
+  (:export
+     :with-gensyms :pipeline
+     :alias :aliases
+     :fn0 :fn1 :fn2 :compose-predicates :aif))
 
-  (defmacro pipeline (expr &rest exprs)
-    "anaphoric macro to create a sequence of expressions where it
+(in-package :macros)
+
+(defmacro with-gensyms ((&rest symbols) &body body)
+  "assigns a gensym for each arg in a let environment"
+  `(let (,@(loop for symbol in symbols
+                 collect `(,symbol (gensym))))
+     ,@body))
+
+(defmacro pipeline (expr &rest exprs)
+  "anaphoric macro to create a sequence of expressions where it
      refers to the result of the previous expression"
-    (if (null exprs)
-        expr
-        `(let ((it ,expr))
-           (pipeline ,@exprs))))
+  (if (null exprs)
+      expr
+      `(let ((it ,expr))
+         (pipeline ,@exprs))))
 
-  (defmacro alias (new-name old-name)
-    "creates an alias to an already defined function or macro"
-    (let ((alias-doc
-            (format nil "~a is an alias for ~a. " new-name old-name))
-          (doc-string (documentation old-name 'function)))
-      `(defmacro ,new-name (&rest args)
-         ,(concatenate 'string alias-doc doc-string)
-         `(,',old-name ,@args))))
-  
-  (defmacro aliases (&rest args)
-    "creates an alias for each pair of arguments"
-    `(progn ,@(loop for pair in (apply #'pairs args)
-                    collect `(alias ,(car pair) ,(cdr pair)))))
-  
-  (defmacro fn0 (&body body)
-    "macro to create a lambda with no arguments"
-    `(lambda () ,@body))
+(defmacro alias (new-name old-name)
+  "creates an alias to an already defined function or macro"
+  (let ((alias-doc
+          (format nil "~a is an alias for ~a. " new-name old-name))
+        (doc-string (documentation old-name 'function)))
+    `(defmacro ,new-name (&rest args)
+       ,(concatenate 'string alias-doc doc-string)
+       `(,',old-name ,@args))))
 
-  (defmacro fn1 (&body body)
-    "anaphoric macro to create a lambda with one _ argument"
-    `(lambda (_) ,@body))
-  
-  (defmacro fn2 (&body body)
-    "anaphoric macro to create a lambda with arguments _1 and _2"
-    `(lambda (_1 _2) ,@body))
+(defmacro aliases (&rest args)
+  "creates an alias for each pair of arguments"
+  `(progn ,@(loop for pair in (apply #'pairs args)
+                  collect `(alias ,(car pair) ,(cdr pair)))))
 
-  (defmacro compose-predicates (expr)
-    "builds a function where every symbol in expr other than 'and and 'or is turned into a proper function call"
-    (let ((logical-operators '(and or not)))
-      (with-gensyms (x)
-        (labels ((transform (p)
-                   (if (member p logical-operators)
-                       p
-                       `(,p ,x))))
-          `(lambda (,x) ,(maptree #'transform expr))))))
+(defmacro fn0 (&body body)
+  "macro to create a lambda with no arguments"
+  `(lambda () ,@body))
 
-  (defmacro aif (condition then &optional else)
-    "anaphoric macro that binds condition value to variable it, which
+(defmacro fn1 (&body body)
+  "anaphoric macro to create a lambda with one _ argument"
+  `(lambda (_) ,@body))
+
+(defmacro fn2 (&body body)
+  "anaphoric macro to create a lambda with arguments _1 and _2"
+  `(lambda (_1 _2) ,@body))
+
+(defmacro compose-predicates (expr)
+  "builds a function where every symbol in expr other than 'and and 'or is turned into a proper function call"
+  (let ((logical-operators '(and or not)))
+    (with-gensyms (x)
+      (labels ((transform (p)
+                 (if (member p logical-operators)
+                     p
+                     `(,p ,x))))
+        `(lambda (,x) ,(maptree #'transform expr))))))
+
+(defmacro aif (condition then &optional else)
+  "anaphoric macro that binds condition value to variable it, which
      can be referenced in both then and else expressions"
-    `(let ((it ,condition))
-       (if it ,then ,else))))
+  `(let ((it ,condition))
+     (if it ,then ,else)))
+
+(modules:module "macros")
+(modules:used-by "aliases" "lazy")
