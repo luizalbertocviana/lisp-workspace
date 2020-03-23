98c697a9094fe16709827770eef65660fe4080e4
Luiz Alberto <luizalbertocviana@gmail.com>
2020-03-23 14:24:38 -0300

    put symbols inside package

--- aliases.lisp
+++ aliases.lisp
@@ -1,8 +1,15 @@
-(defmodule ("aliases"
-            :using   ("macros")
-            :used-by ("lazy"))
+(modules:using "macros")
 
-  (alias with-expressions symbol-macrolet)
-  (alias keep-if          remove-if-not))
+(defpackage :aliases
+  (:use :common-lisp :macros)
+  (:export
+     :with-expressions :keep-if))
 
+(in-package :aliases)
 
+(aliases
+ with-expressions symbol-macrolet
+ keep-if          remove-if-not)
+
+(modules:module "aliases")
+(modules:used-by "lazy")
