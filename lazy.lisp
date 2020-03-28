f5cd3ee3e8f9efb0ae829e3ca1fd5f3c9514afb4
Luiz Alberto <luizalbertocviana@gmail.com>
2020-03-28 01:52:05 -0300

    added some definitions

--- lazy.lisp
+++ lazy.lisp
@@ -2,19 +2,18 @@
 
 (defpackage :lazy
   (:use :common-lisp :aliases :macros)
+  (:shadow :let :cons :car :cdr)
   (:export
-     :thunk :force :lazy-let))
+     :thunk :force :let :cons :car :cdr))
 
 (in-package :lazy)
 
 (defmacro thunk (&body body)
-  "creates a plist containing the code to be evaluated and the
-     result of its evaluation"
+  "creates a plist containing the code to be evaluated and the result of its evaluation"
   ``(:result nil :code ,(fn0 ,@body)))
 
 (defun force (thunk)
-  "force the evaluation of thunk. If already evaluated, just return
-     the result"
+  "force the evaluation of thunk. If already evaluated, just return the result"
   (with-expressions ((code   (getf thunk :code))
                      (result (getf thunk :result)))
     (when code
@@ -22,16 +21,39 @@
       (setf code nil))
     result))
 
-(defmacro lazy-let ((&rest bindings) &body body)
+(defmacro let ((&rest bindings) &body body)
   "creates bindings using implicit thunks"
-  (let ((variables (mapcar #'first bindings))
+  (cl:let ((variables (mapcar #'first bindings))
         (values    (mapcar #'second bindings)))
-    (let ((gvariables (loop for var in variables
+    (cl:let ((gvariables (loop for var in variables
                             collect (gensym))))
-      `(let (,@(loop for gvar in gvariables and val in values
-                     collect `(,gvar (thunk ,@val))))
+      `(cl:let (,@(loop for gvar in gvariables and val in values
+                     collect `(,gvar (thunk ,val))))
          (with-expressions (,@(loop for var in variables and gvar in gvariables
                                     collect `(,var (force ,gvar))))
            ,@body)))))
+
+(defmacro lazily (func &rest args)
+  "calls func with each arg inside of a thunk"
+  `(,func ,@(loop for arg in args
+                  collect `(thunk ,arg))))
+
+(defmacro cons (a b)
+  "creates a cons with both components inside thunks"
+  `(lazily cl:cons ,a ,b))
+
+(defun repeat (a)
+  "creates an infinite list with each element being a"
+  (cons a (repeat a)))
+
+(defmacro defforce (force-fn fn)
+  "creates a thunk-aware version of fn"
+  (let ((doc-string (documentation fn 'function)))
+    `(defun ,force-fn (&rest args)
+       ,doc-string
+       (force (apply #',fn args)))))
+
+(defforce car cl:car)
+(defforce cdr cl:cdr)
 
 (modules:module "lazy")
