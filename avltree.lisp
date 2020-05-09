5d7e39548b9eb5d910ef99f1269383fd6f340652
Luiz Alberto <luizalbertocviana@gmail.com>
2020-05-08 23:50:27 -0300

    added path-to-key and insert

--- avltree.lisp
+++ avltree.lisp
@@ -36,7 +36,37 @@
                           :height height))))))
 
 (defun make-leaf (key val)
-  "returns a avltree node containing key and val, with empty left and right subtrees"
+  "returns a avltree node containing key and val, with empty left and
+right subtrees"
   (transform (bstree:make-leaf key val)))
+
+(defun path-to-key (node key)
+  "returns a list containing references that form a path (in reversed
+order) from node to the node containing key"
+  (do ((current-node node (bstree::with-node current-node
+                              (node-key val left right)
+                            (case (compare key node-key)
+                              (:less    left)
+                              (:equal   nil)
+                              (:greater right))))
+       (path nil (cons current-node path)))
+      ((null current-node) path)))
+
+(defun insert (node key val)
+  "inserts key val pair in avltree rooted at node in case key is not
+present yet"
+  (if node
+      (if (and (avltree-p node)
+               (not (lookup node key)))
+          (let* ((modified-node (bstree:insert node key val))
+                 (path          (path-to-key modified-node key))
+                 ;; as node is not nil, path has at least two nodes
+                 (parent-node   (second path)))
+            (case (compare key (avltree-key parent-node))
+              (:less    (setf (avltree-left  parent-node) (transform (avltree-left  parent-node))))
+              (:greater (setf (avltree-right parent-node) (transform (avltree-right parent-node)))))
+            modified-node)
+          node)
+      (make-leaf key val)))
 
 (modules:module "avltree")
