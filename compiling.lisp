ccd06c933c8eef2356e01354dc89a2cfd128f411
Luiz Alberto <luizalbertocviana@gmail.com>
2020-07-24 22:31:34 -0300

    added documentation to definitions

--- compiling.lisp
+++ compiling.lisp
@@ -10,6 +10,8 @@
 (in-package :compiling)
 
 (defmacro compiling-for (target &body body)
+  "compiles body according to target, which can be one of :speed of
+:safety"
   (let ((speed-declaration
           `((compilation-speed 0)
             (debug             0)
@@ -29,16 +31,24 @@
        ,@body)))
 
 (defmacro disassemble-for (target &body body)
+  "disassemble body as the body of a lambda with no arguments compiled
+according to target"
   `(disassemble (lambda ()
                   (compiling-for ,target ,@body))))
 
 (defmacro with-types ((&rest type-vars) &body body)
+  "given a list of pairs associating types and variables, creates a
+context for body in which variables are declared to be of the
+associated type"
   `(locally
        (declare ,@(loop for type-var in type-vars
                         collect `(type ,@type-var)))
      ,@body))
 
 (defmacro type-let ((&rest type-bindings) &body body)
+  "given a list of triples associating types, variables and values,
+creates a context for body in which both variable and value are
+declared to be of the associated type"
   (let* ((types      (mapcar #'first type-bindings))
          (vars       (mapcar #'second type-bindings))
          (vals       (mapcar #'third type-bindings))
@@ -50,6 +60,10 @@
          ,@body))))
 
 (defmacro type-defun (name (returns &rest type-vars) &optional (doc "") &body body)
+  "creates a function named name with signature and body. signature is
+a list whose first element is the return type of name and the
+remaining ones are pairs of types and input variables. &rest,
+&optional, &key are not supported"
   (let ((vars (mapcar #'second type-vars)))
     `(defun ,name ,vars ,doc
        (with-types ,type-vars
