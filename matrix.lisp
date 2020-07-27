b1c0bd44d85effbf66dc12dccb9037c88b930507
Luiz Alberto <luizalbertocviana@gmail.com>
2020-07-27 02:13:25 -0300

    first version of matrix.lisp

---
+++ matrix.lisp
@@ -0,0 +1,73 @@
+(modules:using "macros")
+
+(defpackage :matrix
+  (:use :common-lisp :macros)
+  (:shadow :aref :identity))
+
+(in-package :matrix)
+
+(defstruct (matrix)
+  "represnets a matrix"
+  (data        nil :type (simple-array))
+  (number-rows nil :type (integer 0))
+  (number-cols nil :type (integer 0)))
+
+(defun new-matrix (&key (type 'number) number-rows number-cols (initial-element 0))
+  "creates a matrix with elements typed to type (defaults to number)
+  whose dimensions are determined by number-rows and number-cols. All
+  positions are initialized as initial-element (defaults to 0)"
+  (make-matrix :data (make-array `(,number-rows ,number-cols)
+                                 :element-type type
+                                 :initial-element initial-element)
+               :number-rows number-rows
+               :number-cols number-cols))
+
+(defun square-matrix (&key (type 'number) dimension (initial-element 0))
+  "creates a square matrix with elements typed to type (defaults to
+  number) and dimension. All positions are initialized as
+  initial-element"
+  (new-matrix :type            type
+              :number-rows     dimension
+              :number-cols     dimension
+              :initial-element initial-element))
+
+(defun identity (&key (type 'number) dimension)
+  "creates an identity matrix"
+  (let ((id (square-matrix :type      type
+                           :dimension dimension)))
+    (dotimes (i dimension)
+      (setf (aref id i i) 1))
+    id))
+
+(defun aref (matrix row col)
+  "element of matrix at position (row col)"
+  (cl:aref (matrix-data matrix) row col))
+
+(defun (setf aref) (value matrix row col)
+  "sets (row col) position of matrix to value"
+  (setf (cl:aref (matrix-data matrix) row col) value))
+
+(defun reduce-two-matrices (op matrix-a matrix-b)
+  "reduces matrix-a and matrix-b applying op position-wise. Result is
+stored in matrix-a"
+  (dotimes (i (matrix-number-rows matrix-a))
+    (dotimes (j (matrix-number-cols matrix-a))
+      (setf (aref matrix-a i j)
+            (funcall op
+                     (aref matrix-a i j)
+                     (aref matrix-b i j)))))
+  matrix-a)
+
+(defun reduce-matrices (op matrix &rest matrices)
+  "reduces matrix and matrices applying op position-wise. Result is
+  stored in matrix"
+  (when matrices
+    (reduce (fn2 (reduce-two-matrices op _1 _2)) matrices
+            :initial-value matrix))
+  matrix)
+
+(defun sum (matrix &rest matrices)
+  "sum matrix and matrices, storing result in matrix"
+  (apply #'reduce-matrices #'+ matrix matrices))
+
+(modules:module "matrix")
