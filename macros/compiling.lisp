9bacb4c4d1a310edf9c5e3f406222a08698805a7
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-04-04 12:12:11 -0300

    refactored defun-spec into spec.lisp

--- macros/compiling.lisp
+++ macros/compiling.lisp
@@ -1,91 +1,10 @@
 (defpackage :compiling
   (:use :common-lisp :macros)
   (:export
-     :defun-spec :compiling-for :disassemble-for
+     :compiling-for :disassemble-for
      :with-types :type-let :type-defun))
 
 (in-package :compiling)
-
-(defmacro defun-spec (name lambda-list (&rest specs) &body body)
-  "defines a function named name with lambda-list as signature.
-Each spec has the form (return-type ((type sig-var) ...)
-:locals ((type local-var) ...)).  For each spec, the types of argument
-values are checked at runtime, and execution is directed to a
-corresponding optimized version of body. In case no spec is met, an
-unoptimized versoin of body is used. For instance, 
-(defun-spec add-one (x)
-    ((fixnum ((fixnum x)) :locals ((fixnum a))))
-  (let ((a 1))
-    (+ x a)))
-expands to
-(defun add-one (x)
-  (cond
-    ((and (typep x 'fixnum))
-     (locally
-         (declare (optimize (speed 3) (safety 0) (debug 0))
-                  (type fixnum x))
-       (the fixnum
-            (progn
-              (let ((a 1))
-                (declare (type fixnum a))
-                (+ x a))))))
-    (t
-     (let ((a 1))
-       (+ x a)))))
-"
-  (labels ((let? (form)
-             (and (listp form)
-                  (>= (length form) 2)
-                  (destructuring-bind (name bindings &body body) form
-                    (declare (ignore body))
-                    (and (or (eq name 'let)
-                             (eq name 'let*))
-                         (every (lambda (binding)
-                                  (and (listp binding)
-                                       (= (length binding) 2))) bindings)))))
-           (treat-let (let-form declaration)
-             (destructuring-bind (let-type bindings &body body) let-form
-               `(,let-type ,bindings ,declaration ,@body)))
-           (treat-spec (spec body)
-             (destructuring-bind (return-type typed-vars &key (locals nil)) spec
-               (labels ((modify-let (let-form)
-                          (let* ((bindings (second let-form))
-                                 (let-variables (mapcar #'first bindings))
-                                 (bound-locals (remove-if-not (lambda (type-var)
-                                                                (member (second type-var)
-                                                                        let-variables))
-                                                              locals))
-                                 (declaration `(declare ,@(loop for (type var) in bound-locals
-                                                                collect `(type ,type ,var)))))
-                            (treat-let let-form declaration))))
-                 (let ((modified-body (lists:map-sexp (lambda (sexp)
-                                                        (if (let? sexp)
-                                                            (modify-let sexp)
-                                                            sexp))
-                                                      body)))
-                   `((and ,@(loop for (type var) in typed-vars
-                                  collect `(typep ,var ',type)))
-                     (locally
-                         (declare (optimize (speed             3)
-                                            (safety            0)
-                                            (debug             0)
-                                            (space             0)
-                                            (compilation-speed 0))
-                                  ,@(loop for (type var) in typed-vars
-                                          collect `(type ,type ,var)))
-                       (the ,return-type (progn ,@modified-body)))))))))
-    (if (stringp (first body))
-        (let ((expanded-body (mapcar #'sb-cltl2:macroexpand-all (rest body))))
-          `(defun ,name ,lambda-list
-             ,(first body)
-             (cond ,@(loop for spec in specs
-                           collect (treat-spec spec expanded-body))
-                   (t ,@(rest body)))))
-        (let ((expanded-body (mapcar #'sb-cltl2:macroexpand-all body)))
-          `(defun ,name ,lambda-list
-             (cond ,@(loop for spec in specs
-                           collect (treat-spec spec expanded-body))
-                   (t ,@body)))))))
 
 (defmacro compiling-for (target &body body)
   "compiles body according to target, which can be one of :speed of
---
+++ macros/spec.lisp
@@ -0,0 +1,144 @@
+(defpackage :spec
+  (:use :common-lisp)
+  (:shadow :defun)
+  (:export :defun :defun-tag))
+
+(in-package :spec)
+
+(defparameter +agressive-optimize-tuple+
+  '(optimize (speed             3)
+             (safety            0)
+             (debug             0)
+             (space             0)
+             (compilation-speed 0))
+  "tuple to be used in (declare ...) forms to indicate optimization settings")
+
+(cl:defun let? (form)
+  "determines whether form is a let-form"
+  (and (listp form)
+       (>= (length form) 2)
+       (destructuring-bind (name bindings &body body) form
+         (declare (ignore body))
+         (and (or (eq name 'let)
+                  (eq name 'let*))
+              (every (lambda (binding)
+                       (and (listp binding)
+                            (= (length binding) 2))) bindings)))))
+
+(cl:defun put-decl-let (let-form declaration)
+  "inserts declaration in let-form"
+  (destructuring-bind (let-type bindings &body body) let-form
+    `(,let-type ,bindings ,declaration ,@body)))
+
+(cl:defun put-type-decl-let (let-form typed-vars)
+  "inserts a (declare (type ...)) form into let-form according to
+typed-vars, verifying which of those are bound by let-form"
+  (let* ((bindings (second let-form))
+         (let-variables (mapcar #'first bindings))
+         (bound-locals (remove-if-not (lambda (type-var)
+                                        (member (second type-var)
+                                                let-variables))
+                                      typed-vars)))
+    (put-decl-let let-form (create-type-declaration bound-locals))))
+
+(cl:defun put-type-decl-all-lets (body typed-vars)
+  "inserts a (declare (type ...)) form into every let form of body
+according to typed-vars, verifying which of those are bound by each
+let form"
+  (lists:map-sexp (lambda (sexp)
+                    (if (let? sexp)
+                        (put-type-decl-let sexp typed-vars)
+                        sexp))
+                  body))
+
+(cl:defun create-type-tuples (typed-vars)
+  "creates (type ...) tuples to a (declare ...) form according to typed-vars"
+  (loop for (type var) in typed-vars
+        collect `(type ,type ,var)))
+
+(cl:defun create-type-declaration (typed-vars)
+  "creates a declaration specifying the type of some variables according to typed-vars"
+  `(declare ,@(create-type-tuples typed-vars)))
+
+(cl:defun create-optimized-body (body return-type typed-vars typed-locals)
+  "creates a (locally ...) form equivalent to body, optimized
+according to typed-vars, typed-locals and return-type"
+  (let ((modified-body (put-type-decl-all-lets body typed-locals)))
+    `(locally
+         (declare ,+agressive-optimize-tuple+
+                  ,@(create-type-tuples typed-vars))
+       (the ,return-type (progn ,@modified-body)))))
+
+(cl:defun treat-spec (spec body)
+  "creates a cond clause responsible for the execution of an optimized
+version of body built according to spec"
+  (destructuring-bind (return-type typed-vars &key (locals nil)) spec
+    `((and ,@(loop for (type var) in typed-vars
+                   collect `(typep ,var ',type)))
+      ,(create-optimized-body body return-type typed-vars locals))))
+
+(cl:defun treat-tagged-spec (tagged-spec body)
+  "creates a case clause responsible for the execution of an optimized
+version of body built according to tagged-spec"
+  (destructuring-bind (tag (return-type typed-vars &key (locals nil))) tagged-spec
+    `(,tag ,(create-optimized-body body return-type typed-vars locals))))
+
+(defmacro defun-tag (name lambda-list (&rest tagged-specs) &body body)
+  (let ((modified-lambda-list (append lambda-list
+                                      (if (member '&key lambda-list)
+                                          '((tag :default))
+                                           '(&key (tag :default))))))
+    (if (stringp (first body))
+        (let ((expanded-body (mapcar #'sb-cltl2:macroexpand-all (rest body))))
+          `(cl:defun ,name ,modified-lambda-list
+             ,(first body)
+             (case tag
+               ,@(loop for tagged-spec in tagged-specs
+                       collect (treat-tagged-spec tagged-spec expanded-body))
+               (:default ,@(rest body)))))
+        (let ((expanded-body (mapcar #'sb-cltl2:macroexpand-all body)))
+          `(cl:defun ,name ,modified-lambda-list
+             (case tag
+               ,@(loop for tagged-spec in tagged-specs
+                       collect (treat-tagged-spec tagged-spec expanded-body))
+               (:default ,@body)))))))
+
+(defmacro defun (name lambda-list (&rest specs) &body body)
+  "defines a function named name with lambda-list as signature.
+Each spec has the form (return-type ((type sig-var) ...)
+:locals ((type local-var) ...)).  For each spec, the types of argument
+values are checked at runtime, and execution is directed to a
+corresponding optimized version of body. In case no spec is met, an
+unoptimized versoin of body is used. For instance, 
+(spec:defun add-one (x)
+    ((fixnum ((fixnum x)) :locals ((fixnum a))))
+  (let ((a 1))
+    (+ x a)))
+expands to
+(defun add-one (x)
+  (cond
+    ((and (typep x 'fixnum))
+     (locally
+         (declare (optimize (speed 3) (safety 0) (debug 0))
+                  (type fixnum x))
+       (the fixnum
+            (progn
+              (let ((a 1))
+                (declare (type fixnum a))
+                (+ x a))))))
+    (t
+     (let ((a 1))
+       (+ x a)))))
+"
+  (if (stringp (first body))
+      (let ((expanded-body (mapcar #'sb-cltl2:macroexpand-all (rest body))))
+        `(cl:defun ,name ,lambda-list
+           ,(first body)
+           (cond ,@(loop for spec in specs
+                         collect (treat-spec spec expanded-body))
+                 (t ,@(rest body)))))
+      (let ((expanded-body (mapcar #'sb-cltl2:macroexpand-all body)))
+        `(cl:defun ,name ,lambda-list
+           (cond ,@(loop for spec in specs
+                         collect (treat-spec spec expanded-body))
+                 (t ,@body))))))
