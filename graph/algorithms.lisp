8f4d4d365b3899b6c70d2bf20f5edd8cc3dbac1c
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-06-07 23:17:01 -0300

    added breadth-first-search to graph system

--- graph/algorithms.lisp
+++ graph/algorithms.lisp
@@ -1,7 +1,7 @@
 (defpackage :graph-algorithms
   (:use :common-lisp)
   (:export
-     :depth-first-search))
+     :depth-first-search :breadth-first-search))
 
 (in-package :graph-algorithms)
 
@@ -26,3 +26,26 @@ visitor-fn at the end of each vertex visit"
                  (funcall visitor-fn vertex)
                  (setf (color vertex) :black)))
         (visit initial-vertex)))))
+
+(defun breadth-first-search (neighbor-fn initial-vertex visitor-fn)
+  "based on the neighbor structure described by neighbor-fn, a
+function that, given a vertex, returns a list of its neighbors,
+performs a breadth-first-search starting at initial-vertex, calling
+visitor-fn at the end of each vertex visit"
+  (let ((vertex-queue (queue:new))
+        (color-ht (make-hash-table)))
+    (macrolet ((color (vertex)
+                 `(gethash ,vertex color-ht :white)))
+      (labels ((touch (vertex)
+                 (queue:enqueue vertex-queue vertex)
+                 (setf (color vertex) :gray)))
+        (touch initial-vertex)
+        (do ()
+            ((queue:empty? vertex-queue) nil)
+          (let* ((current-vertex (queue:dequeue vertex-queue))
+                 (neighbors (funcall neighbor-fn current-vertex)))
+            (loop for neighbor in neighbors
+                  do (when (eq (color neighbor) :white)
+                       (touch neighbor)))
+            (funcall visitor-fn current-vertex)
+            (setf (color current-vertex) :black)))))))
