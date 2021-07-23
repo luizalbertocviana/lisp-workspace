6569c9914dfb795f7caff80601127e4554a449aa
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-07-22 23:02:19 -0300

    added if-let and when-let macros

--- anaphoric-macros/anaphoric-macros.lisp
+++ anaphoric-macros/anaphoric-macros.lisp
@@ -2,7 +2,8 @@
   (:use :common-lisp :macros)
   (:export
    :pipeline
-   :aif :alambda))
+   :aif :if-let :when-let
+   :alambda))
 
 (in-package :anaphoric-macros)
 
@@ -21,6 +22,20 @@ can be referenced in both then and else expressions"
   (with-interned-symbols (it)
     `(let ((,it ,condition))
        (if ,it ,then ,else))))
+
+(defmacro if-let (binding then &optional else)
+  "introduces binding (var test), then executes then in case test is
+not nil, otherwise executes else"
+  (destructuring-bind (var test) binding
+    `(let ((,var ,test))
+       (if ,var
+           ,then
+           ,else))))
+
+(defmacro when-let (binding &body body)
+  "introduces binding (var test), then executes body in case test is
+not nil"
+  `(if-let ,binding (progn ,@body)))
 
 (defmacro alambda (args &body body)
   "anaphoric macro that returns a lambda which can refer to itself as
