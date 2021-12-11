1e62a12aacb313c6f2e515dd82e4520a0236d871
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-12-10 23:27:43 -0300

    corrects a typo

--- graph/digraph.lisp
+++ graph/digraph.lisp
@@ -10,7 +10,7 @@
 (in-package :digraph)
 
 (defstruct digraph
-  "representes a directed graph"
+  "represents a directed graph"
   (num-verts 0 :type fixnum)
   (num-edges 0 :type fixnum)
   (adj #(0) :type (simple-array bit)))
